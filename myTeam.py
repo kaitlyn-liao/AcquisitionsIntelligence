b2740b358dea2848108243305128f34a8ac00ad2
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2022-05-23 19:06:36 -0700

    Add files via upload

--- myTeam.py
+++ myTeam.py
@@ -4,8 +4,8 @@ from pacai.agents.capture.capture import CaptureAgent
 
 
 def createTeam(firstIndex, secondIndex, isRed,
-        first = 'pacai.agents.capture.dummy.DummyAgent',
-        second = 'pacai.student.myTeam.DumbAgent'):
+        first = 'pacai.student.myTeam.MaxAgent',
+        second = 'pacai.student.myTeam.RubyAgent'):
     """
     This function should return a list of two agents that will form the capture team,
     initialized using firstIndex and secondIndex as their agent indexed.
@@ -21,7 +21,7 @@ def createTeam(firstIndex, secondIndex, isRed,
         secondAgent(secondIndex),
     ]
 
-class DumbAgent(CaptureAgent):
+class RubyAgent(CaptureAgent):
     """
     A Dummy agent to serve as an example of the necessary agent structure.
     You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
@@ -37,7 +37,37 @@ class DumbAgent(CaptureAgent):
 
         IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
         """
-        print("init")
+        print('ruby')
+
+        super().registerInitialState(gameState)
+
+        # Your initialization code goes here, if you need any.
+
+    def chooseAction(self, gameState):
+        """
+        Randomly pick an action.
+        """
+        actions = gameState.getLegalActions(self.index)
+        print(actions)
+        return random.choice(actions)
+
+class MaxAgent(CaptureAgent):
+    """
+    A Dummy agent to serve as an example of the necessary agent structure.
+    You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the agent and populates useful fields,
+        such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
+
+        IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
+        """
+        print("max")
 
         super().registerInitialState(gameState)
 
