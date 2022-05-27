7c2c10c10ad32ef788dcb161e9217d1ae041f9a9
Spencer Fulgham <47074040+srfslvr14@users.noreply.github.com>
2022-05-27 16:21:21 -0700

    Add files via upload

--- myTeam.py
+++ myTeam.py
@@ -1,11 +1,12 @@
-from pacai.util import reflection
-import random
-from pacai.agents.capture.capture import CaptureAgent
+from pacai.core.directions import Directions
+
+# from pacai.agents.capture.capture import CaptureAgent
+from pacai.agents.capture.reflex import ReflexCaptureAgent
 
 
 def createTeam(firstIndex, secondIndex, isRed,
-        first = 'pacai.student.myTeam.MaxAgent',
-        second = 'pacai.student.myTeam.RubyAgent'):
+        first = 'pacai.student.myTeam.OffensiveReflexAgent',
+        second = 'pacai.student.myTeam.OffensiveReflexAgent'):
     """
     This function should return a list of two agents that will form the capture team,
     initialized using firstIndex and secondIndex as their agent indexed.
@@ -13,70 +14,91 @@ def createTeam(firstIndex, secondIndex, isRed,
     and will be False if the blue team is being created.
     """
 
-    firstAgent = reflection.qualifiedImport(first)
-    secondAgent = reflection.qualifiedImport(second)
+    firstAgent = OffensiveReflexAgent(firstIndex)
+    secondAgent = DefensiveReflexAgent(secondIndex)
 
     return [
-        firstAgent(firstIndex),
-        secondAgent(secondIndex),
+        firstAgent,
+        secondAgent,
     ]
 
-class RubyAgent(CaptureAgent):
+class DefensiveReflexAgent(ReflexCaptureAgent):
     """
-    A Dummy agent to serve as an example of the necessary agent structure.
-    You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
-    """
-
-    def __init__(self, index, **kwargs):
-        super().__init__(index, **kwargs)
-
-    def registerInitialState(self, gameState):
-        """
-        This method handles the initial setup of the agent and populates useful fields,
-        such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
-
-        IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
-        """
-        print('ruby')
-
-        super().registerInitialState(gameState)
-
-        # Your initialization code goes here, if you need any.
-
-    def chooseAction(self, gameState):
-        """
-        Randomly pick an action.
-        """
-        actions = gameState.getLegalActions(self.index)
-        print(actions)
-        return random.choice(actions)
-
-class MaxAgent(CaptureAgent):
-    """
-    A Dummy agent to serve as an example of the necessary agent structure.
-    You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
+    A reflex agent that tries to keep its side Pacman-free.
+    This is to give you an idea of what a defensive agent could be like.
+    It is not the best or only way to make such an agent.
     """
 
     def __init__(self, index, **kwargs):
-        super().__init__(index, **kwargs)
+        super().__init__(index)
 
-    def registerInitialState(self, gameState):
-        """
-        This method handles the initial setup of the agent and populates useful fields,
-        such as the team the agent is on and the `pacai.core.distanceCalculator.Distancer`.
+    def getFeatures(self, gameState, action):
+        features = {}
 
-        IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
-        """
-        print("max")
+        successor = self.getSuccessor(gameState, action)
+        myState = successor.getAgentState(self.index)
+        myPos = myState.getPosition()
 
-        super().registerInitialState(gameState)
+        # Computes whether we're on defense (1) or offense (0).
+        features['onDefense'] = 1
+        if (myState.isPacman()):
+            features['onDefense'] = 0
 
-        # Your initialization code goes here, if you need any.
+        # Computes distance to invaders we can see.
+        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+        invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
+        features['numInvaders'] = len(invaders)
 
-    def chooseAction(self, gameState):
-        """
-        Randomly pick an action.
-        """
-        actions = gameState.getLegalActions(self.index)
-        print(actions)
-        return random.choice(actions)
+        if (len(invaders) > 0):
+            dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+            features['invaderDistance'] = min(dists)
+
+        if (action == Directions.STOP):
+            features['stop'] = 1
+
+        rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
+        if (action == rev):
+            features['reverse'] = 1
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'numInvaders': -1000,
+            'onDefense': 100,
+            'invaderDistance': -10,
+            'stop': -100,
+            'reverse': -2
+        }
+
+class OffensiveReflexAgent(ReflexCaptureAgent):
+    """
+    A reflex agent that seeks food.
+    This agent will give you an idea of what an offensive agent might look like,
+    but it is by no means the best or only way to build an offensive agent.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
+
+    def getFeatures(self, gameState, action):
+        features = {}
+        successor = self.getSuccessor(gameState, action)
+        features['successorScore'] = self.getScore(successor)
+
+        # Compute distance to the nearest food.
+        foodList = self.getFood(successor).asList()
+
+        # This should always be True, but better safe than sorry.
+        if (len(foodList) > 0):
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = minDistance
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'successorScore': 100,
+            'distanceToFood': -1
+        }
